3897bd29649e2643c669156c36c5579dbf7184c0
Saurav Kumar <sauravkumar080999@gmail.com>
2020-07-31 20:00:27 +0530

    deploy complete

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -28,7 +28,7 @@ const buildControls = (props) => (
 			disabled={!props.purchaseable}
 			onClick={props.ordered}
 		>
-			ORDER NOW
+			{props.isAuth ? 'ORDER NOW' : 'SIGNUP TO ORDER'}
 		</button>
 	</div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,12 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseHandler = () => {
-		this.setState({ purchasing: true });
+		if (this.props.isAuthenticated) {
+			this.setState({ purchasing: true });
+		} else {
+			this.props.onSetAuthRedirectPath('/checkout');
+			this.props.history.push('/auth');
+		}
 	};
 
 	purchaseCancelHandler = () => {
@@ -66,6 +71,7 @@ class BurgerBuilder extends Component {
 						ingredientRemoved={this.props.onIngredientRemoved}
 						disabled={disabledInfo}
 						ordered={this.purchaseHandler}
+						isAuth={this.props.isAuthenticated}
 						purchaseable={this.updatePurchaseState(this.props.ings)}
 					/>
 				</Aux>
@@ -99,6 +105,7 @@ const mapStateToProps = (state) => {
 		ings: state.burgerBuilder.ingredients,
 		price: state.burgerBuilder.totalPrice,
 		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null,
 	};
 };
 
@@ -109,6 +116,8 @@ const mapDispatchToProps = (dispatch) => {
 			dispatch(actions.removeIngredient(ingName)),
 		onInitIngredients: () => dispatch(actions.initIngredients()),
 		onInitPurchase: () => dispatch(actions.purchaseInit()),
+		onSetAuthRedirectPath: (path) =>
+			dispatch(actions.setAuthRedirectPath(path)),
 	};
 };
 
